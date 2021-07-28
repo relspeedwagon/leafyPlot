7e7f6b9573a4087fe25a8e74fe714a90c97fe7b7
Arielle Morabito <74011011+relspeedwagon@users.noreply.github.com>
2021-07-28 13:45:09 -0400

    adds basic form validation for create-plant, create-plot, and start-collection

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,30 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-  }
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//   }
   
-  function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-  }
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+
+// -----------------Bootstrap form validation-------------
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
